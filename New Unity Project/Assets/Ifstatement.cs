97c89a915f4825093caebdacd6e5073dca01cd1c
Chris <shrouded25@gmail.com>
2016-04-22 07:02:11 -0600

    full folder

---
+++ New Unity Project/Assets/Ifstatement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ifstatement : MonoBehaviour 
+{
+	float BallDistance = 100.0f;
+	float toosoonDistance = 4.0f;
+	float toolateDistance = 1.0f;
+
+	// Use this for initialization
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			Swing();
+
+		BallDistance -= Time.deltaTime * 8f;
+	}
+
+	void Swing ()
+	{
+		if (BallDistance > toosoonDistance)
+		{
+			Debug.Log ("Swung too soon");
+		} 
+		else if (BallDistance < toolateDistance) 
+		{
+			Debug.Log ("Swung too late");
+		} else 
+		{
+			Debug.Log ("Homerun");
+		}
+		// Update is called once per frame
+
+		{
+
+		}
+	}
+}
---
+++ New Unity Project/Assets/Loops.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour 
+{   
+	void Start () 
+	{
+		string[] strings = new string[3];
+
+		strings[0] = "Strike one!";
+		strings[1] = "Strike two!";
+		strings[2] = "Strike three! You're OUT!";
+
+		foreach(string item in strings)
+		{
+			print (item);
+		}
+	}
+}
